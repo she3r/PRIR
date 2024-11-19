dcc8bf69c1fbd34ade69ec16e9f22b205bbc5661
Piotr Kubacki <74492669+she3r@users.noreply.github.com>
2024-11-19 19:35:06 +0100

    copy to sequential to parallelSwarm

--- zadanie1/ParallelSwarm.cpp
+++ zadanie1/ParallelSwarm.cpp
@@ -28,3 +28,99 @@ double ParallelSwarm::get_position(int robot, int dimension) {
 
 }
 
+void ParallelSwarm::single_step() {
+  step++;
+  evaluate_function();
+  find_neighbours_and_remember_best();
+  move();
+  fit_antenna_range();
+}
+
+void ParallelSwarm::evaluate_function() {
+  for (int robot = 0; robot < robots; robot++) {
+    function_value[robot] = function->value(position[robot]);
+  }
+}
+
+void ParallelSwarm::find_neighbours_and_remember_best() {
+  for (int robot = 0; robot < robots; robot++) {
+    best_id = robot;
+    nearest_neighbours[robot] = 0;
+
+    best_function_value = function_value[robot];
+    my_antenna_range_sq = antenna_range_sq[robot];
+    my_position = position[robot];
+
+    for (int other_robot = 0; other_robot < robot; other_robot++) {
+      compare_with_other_robot(robot, other_robot);
+    }
+    for (int other_robot = robot + 1; other_robot < robots; other_robot++) {
+      compare_with_other_robot(robot, other_robot);
+    }
+
+    neighbour_id[robot] = best_id;
+  }
+}
+
+void ParallelSwarm::compare_with_other_robot(int robot, int other_robot) {
+  if (MathHelper::distanceSQ(my_position, position[other_robot], dimensions) <
+      my_antenna_range_sq) {
+    // inny robot jest w zasięgu anteny
+    nearest_neighbours[robot]++;
+
+    if (best_function_value < function_value[other_robot]) {
+      // w dodatku ma lepszą wartość funkcji
+      best_function_value = function_value[other_robot];
+      best_id = other_robot;
+    }
+  }
+}
+
+void show(double *pos, double *pos2, double *pos3, int size) {
+  for (int i = 0; i < size; i++) {
+    cout << "r1 " << pos[i] << " vs r2 " << pos2[i] << " vs r3 " << pos3[i]
+         << endl;
+  }
+}
+
+void ParallelSwarm::move() {
+  for (int robot = 0; robot < robots; robot++) {
+    MathHelper::move(position[robot], position[neighbour_id[robot]],
+                     new_position[robot], dimensions, STEP_SIZE / sqrt(step));
+  }
+  for (int robot = 0; robot < robots; robot++)
+    for (int d = 0; d < dimensions; d++)
+      position[robot][d] = new_position[robot][d];
+}
+
+void ParallelSwarm::allocate_memory() {
+  position = new double *[robots];
+  new_position = new double *[robots];
+  for (int i = 0; i < robots; i++) {
+    position[i] = new double[dimensions];
+    new_position[i] = new double[dimensions];
+  }
+
+  neighbour_id = new int[robots];
+  nearest_neighbours = new int[robots];
+  function_value = new double[robots];
+  antenna_range_sq = new double[robots];
+}
+
+void ParallelSwarm::fit_antenna_range() {
+  double range;
+  for (int robot = 0; robot < robots; robot++) {
+    range = antenna->range(sqrt(antenna_range_sq[robot]),
+                           nearest_neighbours[robot]);
+    antenna_range_sq[robot] = range * range;
+  }
+}
+
+void ParallelSwarm::initialize_antennas() {
+  double vSQ = antenna->initial_range();
+  vSQ *= vSQ;
+  for (int r = 0; r < robots; r++) {
+    antenna_range_sq[r] = vSQ;
+  }
+}
+
--- zadanie1/ParallelSwarm.h
+++ zadanie1/ParallelSwarm.h
@@ -11,7 +11,24 @@
 #include "Swarm.h"
 class ParallelSwarm: public Swarm {
 private:
+    int best_id;
+    int step;
+    double best_function_value;
+    double my_antenna_range_sq;
+    double *my_position;
+    int *neighbour_id; // identyfikator najbliższego sąsiada
+    int *nearest_neighbours; // liczba sąsiadów w zasięgu
+    double *antenna_range_sq; // kwadrat zasięgu anteny
+    double *function_value;  // wartość funkcji
 
+    void single_step();
+    void evaluate_function();
+    void find_neighbours_and_remember_best();
+    void compare_with_other_robot( int robot, int other_robot );
+    void move();
+    void fit_antenna_range();
+    void allocate_memory();
+    void initialize_antennas();
 public:
     ParallelSwarm( int robots, Antenna *antenna, Function *function  );
     void run( int steps ) override;
