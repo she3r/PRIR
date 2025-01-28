c48dce63f302b57900342d1c9e4c39ad006ed97c
Piotr Kubacki <74492669+she3r@users.noreply.github.com>
2025-01-28 17:58:09 +0100

    clear repo

--- zadanie1/Swarm.cpp
+++
@@ -1,8 +0,0 @@
-#include"Swarm.h"
-
-Swarm::Swarm( int robots, Antenna *antenna, Function *function ) {
-    this->robots = robots;
-    this->antenna = antenna;
-    this->function = function;
-    this->dimensions = function->dimensions();
-}
--- zadanie2/ThreadAndPosition.java
+++
@@ -1,9 +0,0 @@
-/**
- * Para wątek oraz jego położenie startowe.
- * 
- * @param thread wątek uprawniony do odczytu danych z tablicy
- * @param position pierwsza pozycja do odczytu danych z tablicy dla wątku thread
- */
-public record ThreadAndPosition( Thread thread, Position2D position ) {
-
-}
